0ae436fb1c18438004e8efffcae6af2a6bfaa469
Victor jimenez <vhbj149601@gmail.com>
2016-10-24 10:10:22 -0500

    Added new features

--- productos/urls.py
+++ productos/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url 
 from . import views
 app_name = 'productos'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
-    url(r'^/create/$', views.DetailView.as_view(), name='create'),  
-    url(r'^/delete/$', views.DetailView.as_view(), name='delete'),  
-    url(r'^/update/$', views.DetailView.as_view(), name='uupdate'),        
+    url(r'producto/new/$',views.CreateProduct.as_view(), name='new'),  
+    url(r'producto/(?P<pk>[0-9]+)/$', views.UpdateProduct.as_view( ) , name='update'),  
+    url(r'producto/(?P<pk>[0-9]+)/delete', views.DeleteProduct.as_view( ) , name='delete'),        
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+from django.views.generic.edit import CreateView ,  DeleteView , UpdateView 
+from django.core.urlresolvers import reverse_lazy
 from .models import Producto
 class IndexView(generic.ListView):
     template_name = 'productos/productos.template.html'
@@ -6,5 +8,14 @@ class IndexView(generic.ListView):
         return Producto.objects.all()
 
 class DetailView(generic.DetailView):
-    model = Producto
-    template_name = 'productos/details.template.html'    
+      model = Producto
+      template_name = 'productos/details.template.html'    
+class CreateProduct(generic.CreateView):
+      model = Producto
+      fields = ['nombre_producto', 'tipo_producto' , 'marca_producto' , 'precio_unitario_producto', 'stock_producto' , 'imagen_producto'] 
+class UpdateProduct(generic.UpdateView):
+      model = Producto
+      fields = ['nombre_producto' , 'precio_unitario_producto', 'stock_producto' ]
+class DeleteProduct(generic.DeleteView):
+      model = Producto
+      success_url = reverse_lazy('productos:index')
