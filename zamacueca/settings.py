0ad04f001ff1f6ddb9af67cdd3cb1438a8ad9ac6
Victor jimenez <vhbj149601@gmail.com>
2016-10-30 14:08:01 -0600

    refactoring profile

--- zamacueca/settings.py
+++ zamacueca/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
