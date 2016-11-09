306f4ef23d4ee01b97c0b440c34bd67ca3e8810b
Victor Hugo Barrios Jiménez <vhbj149601@gmail.com>
2016-11-08 20:34:47 -0600

    Updating migrations

--- cita/admin.py
+++ cita/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+"""
 from .models import Cita
 # Register your models here.
 class CitasModelAdmin(admin.ModelAdmin):
@@ -6,3 +7,4 @@ class CitasModelAdmin(admin.ModelAdmin):
     class Meta:
         model = Cita
 admin.site.register(Cita ,CitasModelAdmin)
+"""
--- cita/models.py
+++ cita/models.py
@@ -7,7 +7,3 @@ class Producto(models.Model):
     precio  =  models.IntegerField()
     stock = models.IntegerField()
     descripcion  = models.CharField(max_length = 250)
-class Cita(models.Model):
-    nombre = models.CharField(max_length = 150)
-    direccion  = models.CharField(max_length = 150) 
-    fecha = models.DateTimeField()
