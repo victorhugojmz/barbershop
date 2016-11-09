a979da92681c373b484f21817062e941a11d6e73
Victor Hugo Barrios Jiménez <vhbj149601@gmail.com>
2016-11-08 21:00:23 -0600

    Upgrading the delete form

--- cita/admin.py
+++ cita/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Cita
 """
 from .models import Cita
 # Register your models here.
@@ -8,3 +9,4 @@ class CitasModelAdmin(admin.ModelAdmin):
         model = Cita
 admin.site.register(Cita ,CitasModelAdmin)
 """
+admin.site.register(Cita)
