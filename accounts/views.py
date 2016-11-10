dc9777b851418ed60af2db30cb9ba05c97673e50
Victor Hugo Barrios Jiménez <vhbj149601@gmail.com>
2016-11-10 01:29:18 -0600

    Upgrading futurre security dumb issues

--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render , redirect
 from django.contrib.auth import (authenticate, login , get_user_model,logout)
 from .forms import UserLoginForm , UserRegistrationForm
 def login_user(request):
-    title = "Login"
-    form = UserLoginForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(username = username , password  = password)
-        login(request, user)
-        return render(request,"index/index.template.html",{})  
-    return render(request,"account/login.html",{"form": form, "title": title })
+        if not request.user.is_authenticated():
+            form = UserLoginForm(request.POST or None)
+            if form.is_valid():
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+                user = authenticate(username = username , password  = password)
+                login(request, user)
+                return render(request,"index/index.template.html",{})  
+            return render(request,"account/login.html",{"form": form })
+        else: 
+            return render(request,"index/index.template.html",{}) 
 def logout_user(request): 
     logout(request)
     return render(request,"account/logout.html", {})
