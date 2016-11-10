34ff603a8d091ae20c7f7d550c28e5f4586c0502
Victor Hugo Barrios Jiménez <vhbj149601@gmail.com>
2016-11-10 16:01:31 -0600

    Updating the form validation

--- cita/forms.py
+++ cita/forms.py
@@ -1,15 +1,19 @@
 from django import forms
 from models  import Cita
 class CitaForm(forms.Form):
-    nombre = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre'}))
-    direccion = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre'}))
+    nombre_cliente = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre'}))
+    telefono_cliente = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Telefono'}))
+    direccion = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Direccion'}))
     fecha = forms.DateField()
+    def post(nombre,direccion,fecha):
+        model = Cita
+        save(nombre,direccion,fecha)
     class Meta: 
         model = Cita
         fields = [ 
-            'nombre',
             'nombre_cliente',
             'telefono_cliente',
             'direccion',
             'fecha',
         ]
+    
--- cita/models.py
+++ cita/models.py
@@ -3,9 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 class Cita(models.Model):
     cita_id = models.AutoField(primary_key= True)
-    nombre = models.CharField(max_length = 150)
     fecha = models.DateField()
     nombre_cliente = models.CharField(max_length = 150)
-    telefono_cliente =  models.CharField(max_length = 12 )
+    telefono_cliente =  models.CharField(max_length = 12)
     direccion = models.CharField(max_length = 120)
-    
