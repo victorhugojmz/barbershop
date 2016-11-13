c992fcf872c41686020adc3dffec11b192aaa19d
Victor Hugo Barrios Jiménez <vhbj149601@gmail.com>
2016-11-13 07:02:44 -0600

    Admin Customized for date-booking app

--- cita/admin.py
+++ cita/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import Cita
-admin.site.register(Cita)
+class CitasModelAdmin(admin.ModelAdmin):
+    list_display = ['cita_id','fecha','nombre_cliente','telefono_cliente']
+    list_filter = ['fecha']
+    class Meta:
+        model = Cita
+admin.site.register(Cita,CitasModelAdmin)
