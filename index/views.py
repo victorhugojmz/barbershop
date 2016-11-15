b8d52d6ac3fb5ff348752873bcce30f5357eebee
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-14 23:02:10 -0600

    Refactoring

--- index/views.py
+++ index/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render,  get_object_or_404, redirect  
+from django.shortcuts import (  
+                                render,
+                                redirect
+                             )
+from django.views import generic 
+from django.views.generic import View
+from django.views.generic.edit import (
+                                        CreateView , 
+                                        DeleteView , 
+                                        UpdateView
+                                      )
+from .models import (
+                        Barbero ,
+                        Galeria
+                    )
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
 from django.template import loader
-from django.views import generic 
-from django.views.generic import View
-from django.views.generic.edit import CreateView ,  DeleteView , UpdateView
-from .models import Barbero , Galeria , Producto
+
+#from .models import,Producto
 from django.contrib.auth import authenticate , login , logout , update_session_auth_hash
-from .forms import UpdateProductForm ,CreateProductForm
+#
 class IndexView(generic.ListView):
     template_name = 'index/productos.template.html'
     def get_queryset(self):
--- productos/views.py
+++ productos/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import (  
+                                render,
+                                redirect
+                             )
+from django.views import generic 
+from django.views.generic import View
+from django.views.generic.edit import (
+                                        CreateView , 
+                                        DeleteView , 
+                                        UpdateView
+                                      )
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse_lazy
 from django.template import loader
-from django.views import generic 
-from django.views.generic import View
-from django.views.generic.edit import CreateView ,  DeleteView , UpdateView
-from .models import Barbero , Galeria , Producto
-from django.contrib.auth import authenticate , login , logout , update_session_auth_hash
-from .forms import UpdateProductForm ,CreateProductForm
+from .models import Producto
+from .forms import (  
+                    UpdateProductForm,
+                    CreateProductForm
+                   )
 class IndexView(generic.ListView):
     template_name = 'index/productos.template.html'
     def get_queryset(self):
