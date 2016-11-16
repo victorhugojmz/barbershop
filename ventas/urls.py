44e11bfaa4d4a3a85d8181e1a459b027d52a4143
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-15 19:20:29 -0600

    Created html for sales

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,1 +1,6 @@
 from django.conf.urls import include , url
+from . import views
+app_name = 'productos'
+urlpatterns  = [
+    url(r'^$',, name='index'),
+]
--- ventas/views.py
+++ ventas/views.py
@@ -1,1 +1,3 @@
 from django.shortcuts import render
+def index_citas(request):
+    return render(request, 'ventas/index.ventas.html',{})
