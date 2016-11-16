b47a1eca7861b34be312888316611f8c90d3ab4d
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-15 19:35:07 -0600

    Url and view created

--- ventas/urls.py
+++ ventas/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include , url
 from . import views
 app_name = 'productos'
 urlpatterns  = [
-    url(r'^$',, name='index'),
+    url(r'^$', views.index_citas , name='index'),
 ]
