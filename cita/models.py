a4d072575bc28c888b53c01a515237a6361d654a
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-18 05:09:08 -0600

    Updating models

--- cita/models.py
+++ cita/models.py
@@ -2,8 +2,8 @@ from __future__ import unicode_literals
 from django.db import models
 class Cita(models.Model):
     cita_id = models.AutoField(primary_key= True)
-    fecha_cita = models.DateField()
+    fecha_cita = models.DateTimeField()
+    fecha_creacion_cita = models.DateField('Creacion', auto_now= True , blank=True)
     nombre_cliente = models.CharField(max_length = 150)
     telefono_cliente =  models.CharField(max_length = 20)
     direccion = models.CharField(max_length = 120)
-    
