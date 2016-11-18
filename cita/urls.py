c481bd44191b4fbfe246b2fbbc7710c031f4e02e
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-18 04:04:59 -0600

    little mistakes solved

--- cita/urls.py
+++ cita/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'nueva/$', views.book , name='book'),
     url(r'api/citas/$', views.ListaDeCitas.as_view(),name='get-citas'),
     url(r'editar/(?P<pk>[0-9]+)/$', views.UpdateCitaView.as_view(), name='update-cita'),
-    url(r'eliminar/(?P<pk>[0-9]+)/$', view.DeleteCita.as_view(), name='delete-cita'),
+    url(r'eliminar/(?P<pk>[0-9]+)/$', views.DeleteCita.as_view(), name='delete-cita'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
