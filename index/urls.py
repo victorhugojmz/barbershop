02223017e7ef0298d811856aff085013524abcd8
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-23 10:43:20 -0600

    updating urls

--- index/urls.py
+++ index/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = 'index'
 urlpatterns = [
     url(r'^$', views.index , name='zamacueca'),
-    url(r'^about/$', views.about, name='about'),
+    url(r'^nosotros/$', views.about, name='about'),
     url(r'^galeria/$',  views.gallery, name='gallery'),
 ] 
--- productos/urls.py
+++ productos/urls.py
@@ -5,6 +5,6 @@ urlpatterns  = [
     url(r'^$', views.IndexView, name='index'),
     url(r'producto/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'nuevo/$' , views.ProductCreate.as_view( ), name="product-add"), 
-    url(r'producto/update/(?P<pk>[0-9]+)/$' , views.ProductUpdate.as_view( ), name="product-update"), 
-    url(r'producto/(?P<pk>[0-9]+)/delete/$' , views.ProductDelete.as_view( ), name="product-delete"),
+    url(r'producto/actualizar/(?P<pk>[0-9]+)/$' , views.ProductUpdate.as_view( ), name="product-update"), 
+    url(r'producto/(?P<pk>[0-9]+)/eliminar/$' , views.ProductDelete.as_view( ), name="product-delete"),
 ]
