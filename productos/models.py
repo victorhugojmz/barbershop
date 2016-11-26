8014b88a98b2a4e6cd2fb7cc8c4b9efa9a468d45
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-26 00:38:38 -0600

    Models updated

--- productos/models.py
+++ productos/models.py
@@ -2,12 +2,24 @@ from __future__ import unicode_literals
 from django.db import models
 # Create your models here.
 class Producto(models.Model):
-    id_producto = models.AutoField(primary_key= True)
-    nombre_producto = models.CharField(max_length = 150)
-    tipo_producto = models.CharField(max_length = 50)
-    marca_producto =  models.CharField(max_length = 50)
-    precio_unitario_producto = models.IntegerField(default= 0)
-    stock_producto = models.IntegerField(default=0)
+    id_producto = models.AutoField(
+                                primary_key= True
+                                )
+    nombre_producto = models.CharField(
+                                max_length = 150
+                                )
+    tipo_producto = models.CharField(
+                                max_length = 50
+                                )
+    marca_producto =  models.CharField(
+                                max_length = 50
+                                )
+    precio_unitario_producto = models.IntegerField(
+                                default= 0
+                                )
+    stock_producto = models.IntegerField(
+                                default=0
+                                )
     descripcion_producto = models.TextField()
     imagen_producto = models.FileField()
     def get_absolute_url(self):
