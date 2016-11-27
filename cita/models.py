35db4b8210018db581b3de3e440a1fd580caea2a
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-27 16:30:08 -0600

    models.py

--- cita/models.py
+++ cita/models.py
@@ -1,10 +1,33 @@
 from __future__ import unicode_literals
 from django.db import models
+import datetime 
 class Cita(models.Model):
     cita_id = models.AutoField(primary_key= True)
-    fecha_cita = models.DateField(blank=False, null=False)
-    fecha_creacion_cita = models.DateTimeField('Creacion', auto_now= True , blank=True)
-    nombre_cliente = models.CharField(max_length = 150)
-    telefono_cliente =  models.CharField(max_length = 20)
-    direccion = models.CharField(max_length = 120)
-    hora_cita = models.TimeField()
+    fecha_cita = models.DateField(
+                        blank=False, 
+                        null=False
+                        )
+    fecha_creacion_cita = models.DateTimeField(
+                        auto_now=False,
+                        auto_now_add= True 
+                        )
+    nombre_cliente = models.CharField(
+                        max_length = 150,
+                        blank=False, 
+                        null=False
+                        )
+    telefono_cliente =  models.CharField(
+                        max_length = 20,
+                        blank=False, 
+                        null=False
+                        )
+    direccion = models.CharField(
+                        max_length = 120,
+                        blank=True, 
+                        null=True
+                        )
+    hora_cita = models.TimeField(
+                        'Hora',
+                        blank=False, 
+                        null=False
+                        )
--- zamacueca/settings.py
+++ zamacueca/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'zamacueca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'barberia',
+        'NAME': 'zamacueca_barbershop',
         'USER': 'zamacueca',
         'PASSWORD': '',
         'HOST': 'localhost',
