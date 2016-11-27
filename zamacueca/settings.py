a76ea391f4589ffa41c1aec51a9be86f523b4402
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-26 22:30:08 -0600

    deleted migrations

--- zamacueca/settings.py
+++ zamacueca/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'zamacueca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'zamacueca',
+        'NAME': 'barberia',
         'USER': 'zamacueca',
         'PASSWORD': '',
         'HOST': 'localhost',
