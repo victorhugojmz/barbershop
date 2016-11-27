4af27a7130e4e541ee0ae5960a971a323521d441
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-27 03:30:47 -0600

    models

--- cita/models.py
+++ cita/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 class Cita(models.Model):
     cita_id = models.AutoField(primary_key= True)
-    fecha_cita = models.DateTimeField()
+    fecha_cita = models.DateTimeField(blank=False, null=False)
     fecha_creacion_cita = models.DateField('Creacion', auto_now= True , blank=True)
     nombre_cliente = models.CharField(max_length = 150)
     telefono_cliente =  models.CharField(max_length = 20)
