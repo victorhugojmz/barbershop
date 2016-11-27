f5788066dd4420a85a01e8e38b4a8e48bc3e39af
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-27 02:53:33 -0600

    updated models

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Venta , VentaProducto
 # Register your models here.
+class VentasModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Venta
+        list_display = ['id_venta','fecha_venta','total_venta']
+        list_filter = ['fecha_venta']
+admin.site.register(Venta,VentasModelAdmin)
+admin.site.register(VentaProducto)
--- ventas/models.py
+++ ventas/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from productos.models import Producto
+from index.models import Servicio
 # Create your models here.
+class Venta(models.Model):
+    id_venta = models.AutoField(primary_key=True)
+    fecha_venta = models.DateTimeField('Creacion',auto_now_add=True,blank=True)
+    total_venta = models.DecimalField(max_digits=6, decimal_places=2)
+class VentaProducto(models.Model):
+    id_venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto)
+    cantidad = models.IntegerField()
+    subtotal = models.DecimalField(max_digits=6, decimal_places=2)
+    
