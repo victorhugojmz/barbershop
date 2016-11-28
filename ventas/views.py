3d4867c9ff8b3eb9273332a5f40e1f5dae5908f9
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-27 20:16:22 -0600

    creating basic view

--- ventas/views.py
+++ ventas/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 def index_citas(request):
     return render(request, 'ventas/index.ventas.html',{})
+def genera_venta(request):
+    if not request.user.is_authenticated:
+        return render(request,'index/index.template.html',{})
+    else:    
+        form = VentaForm(request.POST or None)
+        if form.is_valid():
+            venta = form.save(commit=False)
+            venta.save()
+            return render(request, 'music/detail.html', {'venta': venta })
+        context = {
+            "form": form
+        }
+        return render(request,'ventas/venta_form.html',context)
