3a798ceaecc1f0982e2fe728b8e8857d60bef8b4
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-12-01 10:16:02 -0600

    added login required decorator

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include , url
+from django.contrib.auth.decorators import login_required
 from . import views
 app_name = 'ventas'
 urlpatterns  = [
-    url(r'^$', views.index_citas , name='index'),
-    url(r'nueva/$', views.genera_venta,name='sale-create'),   
+    url(r'^$', login_required(views.index_citas) , name='index'),
+    url(r'nueva/$', login_required(views.genera_venta),name='sale-create'),   
 ]
