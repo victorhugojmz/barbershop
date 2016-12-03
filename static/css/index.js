bdae7e940e8bf0b949914a0f4db650a63dc3b223
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-12-03 01:46:30 -0600

    creating image grid

--- static/css/index.js
+++ static/css/index.js
@@ -1,7 +1,20 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 19.3513955, lng: -99.1504914},
+    zoom: 16
+  });
+  var icon = {
+        url : 'static/images/calavera-final.ico',
+        scaledSize: new google.maps.Size(90, 90),
+        origin: new google.maps.Point(0,0), 
+        anchor: new google.maps.Point(0,0) 
+  }
+  var marker = new google.maps.Marker({
+    position: {lat: 19.3513955, lng: -99.1504914},
+    map: map,
+    title: 'Hello World!',
+    icon: icon
   });
 }
+marker.setMap(map);
