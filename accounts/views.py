0697fb95f49cdeed454a2d3631b2dc84ea5ec740
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-12-04 01:57:58 -0600

    changes

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,8 @@ from django.contrib.auth import (authenticate, login , get_user_model,logout)
 from django.core.mail import send_mail
 from django.http import HttpResponse
 from .forms import UserLoginForm , UserRegistrationForm
-from cita.models import Cita
+from ventas.models import Venta
+from decimal import Decimal
 #Login User View 
 def login_user(request):
         if not request.user.is_authenticated():
@@ -63,7 +64,7 @@ def register_user(request):
     return render(request, 'account/registration_form.html', context)
 #View that generates PDF reports
 def user_reports(request): 
-    query = Cita.objects.filter(fecha_cita__year='2016')
+    query = Venta.objects.all()
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment;filename=Reporte.pdf'
     buffer = BytesIO()
@@ -74,10 +75,35 @@ def user_reports(request):
     c.setFont('Helvetica-Bold',20)
     c.drawString(30,475,'Reporte de ventas')
     c.setFont('Helvetica',18)
-    c.drawString(610,475,"Fecha: 26/11/2016")
+    c.drawString(610,475,"Diciembre")
     c.line(30,460,760,460)
+    c.setFont('Helvetica-Bold',20)
+    altura = 420
+    c.drawString(30+10,altura,"Barbero")
+    c.drawString(200+10,altura,"Total")
+    c.drawString(340+10,altura,"Fecha")
+    c.drawString(460+10,altura,"Servicio")
+    c.drawString(600+10,altura,"Comision")
+    c.setFont('Helvetica',18)
+    altura_inicial = 380
+    two_d = Decimal(10) ** -1
+    linea = 30
+    linea_final = linea + 700
+    suma= 0 
+    comision = 0
+    comisiones= 0
     for q in query:
-        c.drawString(100,300, str(q))
+        comision = Decimal(q.total_venta).quantize(two_d) * Decimal(0.20).quantize(two_d)
+        c.drawString(30,altura_inicial,str(q.barbero))
+        c.drawString(200,altura_inicial,'$'+ str(q.total_venta) + 'MXN')
+        c.drawString(340,altura_inicial,str(q.fecha_venta.date()))
+        c.drawString(460,altura_inicial,str(q.servicio))
+        c.drawString(600,altura_inicial,'$'+ str(comision)+ 'MXN')
+        c.line(linea,altura_inicial-10,linea_final,altura_inicial-10)
+        altura_inicial -=30
+        suma += int(q.total_venta) 
+        comisiones += int(comision) 
+    c.drawString(30,altura_inicial-20,"Ganancias Netas " + "$" + str(suma-comisiones)+ "MXN")
     c.showPage()
     c.save()
     pdf = buffer.getvalue()
