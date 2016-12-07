2f2e266a27f6da93765d35c006e816cec35e12c5
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-12-07 14:40:34 -0600

    updating UI

--- static/css/index.js
+++ static/css/index.js
@@ -1,6 +1,3 @@
-var form = document.getElementById("salidaform");
-         showForm function ( )
-         form.style.display = 'none';
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
